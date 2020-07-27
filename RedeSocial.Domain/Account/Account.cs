b0bf8527d9864396a6790f14b989ffe962195370
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-07-27 08:23:52 -0300

    Add project files.

---
+++ RedeSocial.Domain/Account/Account.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedeSocial.Domain.Account
+{
+    public class Account : IdentityUser
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DtBirthday { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
